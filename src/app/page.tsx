7648d95544e77fefa0eb4961c833d11acba5a094
funayamateppei <t.funayama0217@gmail.com>
2023-02-08 23:06:09 +0900

    Typescriptで型指定

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,40 @@
 "use client";
 
-import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "./page.module.css";
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
+interface ImageUrl {
+  file: string,
+}
 
 export default function Home() {
 
-  const [img, setImg] = useState('');
+  const [img, setImg] = useState("");
 
-  const fetchCatImage = async () => {
+  useEffect()
+
+  const fetchCatImage = async (): Promise<ImageUrl> => {
     const response = await axios.get("https://aws.random.cat/meow");
-    const imgPath = response.data.file;
-    console.log(imgPath);
-    setImg(imgPath);
+    console.log(response);
+    const result = response.data;
+    console.log(result);
+    return result;
   };
+
+  const handleClick = async () => {
+    const src = await fetchCatImage()
+    setImg(src.file);
+  }
 
   return (
     <main className={styles.main}>
       <h1>猫画像アプリ</h1>
       <img src={img} alt="cat" width={500} height="auto" />
-      <button onClick={fetchCatImage}>今日のヌコ</button>
+      <button onClick={handleClick}>今日のヌコ</button>
     </main>
   );
 }
